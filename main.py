bf99404350f6a7b2dced7e919c424354a6f3648b
ankit6370232757 <ankitkumarrath5@gmail.com>
2025-09-04 09:35:11 +0530

    python

--- main.py
+++ main.py
@@ -181,25 +181,25 @@
 # else:
 #     print("its a normal year")
 
-t = int(input("please tell the temprature :- "))
+# t = int(input("please tell the temprature :- "))
 
-if t < 0:
-    print("Freezing cold")
+# if t < 0:
+#     print("Freezing cold")
 
-elif t >= 0 and t <10:
-    print("very cold")
+# elif t >= 0 and t <10:
+#     print("very cold")
 
-elif t >= 10 and t <20:
-    print("cold")
+# elif t >= 10 and t <20:
+#     print("cold")
 
-elif t >= 20 and t <30:
-    print("plesant")
+# elif t >= 20 and t <30:
+#     print("plesant")
 
-elif t >= 30 and t <40:
-    print("hot")
+# elif t >= 30 and t <40:
+#     print("hot")
 
-else:
-    print("temprature is very hot ")
+# else:
+#     print("temprature is very hot ")
 
 
 
